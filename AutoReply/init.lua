2edeb1db138d1f605f228e38ea7ff32b13fc88ca
Bernardo Meurer <bernardo_meurer@hotmail.com>
2015-05-16 18:47:58 -0300

    Updated instructions

--- AutoReply/init.lua
+++ AutoReply/init.lua
@@ -3,7 +3,7 @@ haspoked = {} --To index individual users.
 poketime = {} --To save poke times.
 maxpokes = 2 --Max number of pokes minus 1 (In this case 3;3-1=2).
 maxtime = 180 --Interval in seconds in which the pokes will be accounted for.
-doreply = 1
+doreply = "y"
 recmsg = "Received your poke! Please don't poke me again, I'll respond whenever I can." --Message to be sent on poke receival.
 kickmsg = "Don't abuse pokes!" --Message to be sent with kick.
 function onClientPokeEvent(serverConnectionHandlerID, pokerID, pokerName, message, ffIgnored)
@@ -11,7 +11,7 @@ function onClientPokeEvent(serverConnectionHandlerID, pokerID, pokerName, messag
 			haspoked[pokerID] = pokerID --Creates a record
 			count[pokerID] = 1 --Sets his poke count to one
 			poketime[pokerID] = os.time() --Records the time of first poke
-			if (doreply == 1) then
+			if (doreply == "y") then
 				ts3.requestSendPrivateTextMsg(serverConnectionHandlerID, recmsg, pokerID) --Send message back.
 			end
 	elseif(pokerID == haspoked[pokerID])then --If there is a record already
@@ -22,7 +22,7 @@ function onClientPokeEvent(serverConnectionHandlerID, pokerID, pokerName, messag
 				if(os.difftime(poketime[pokerID],os.time()) > maxtime) then --If the interval between pokes is greater than maxtime.
 					poketime[pokerID] = os.time --Reset timer.
 				end
-				if (doreply == 1)then
+				if (doreply == "y")then
 				  ts3.requestSendPrivateTextMsg(serverConnectionHandlerID, recmsg, pokerID) --Sends message.
 				end
 				count[pokerID] = count[pokerID]+1 --Increments poke count for the user.
