4ee74c49eacd0886857244fac7f2c385534301c0
Bernardo Meurer <meurerbernardo@gmail.com>
2015-10-12 23:29:10 -0300

    Rolled back to previous version that actually worked.

--- AutoReply/init.lua
+++ AutoReply/init.lua
@@ -1,32 +1,24 @@
-count = {} --To keep track of poke count.
-haspoked = {} --To index individual users.
-poketime = {} --To save poke times.
-maxpokes = 3 --Max number of pokes.
-maxtime = 180 --Interval in seconds in which the pokes will be accounted for.
-doreply = "y"
-recmsg = "Received your poke! Please don't poke me again, I'll respond whenever I can." --Message to be sent on poke receival.
-kickmsg = "Don't abuse pokes!" --Message to be sent with kick.
+count = {}
+haspoked = {}
+poketime = {}
+maxpokes = 2
+maxtime = 180
 function onClientPokeEvent(serverConnectionHandlerID, pokerID, pokerName, message, ffIgnored)
-	maxpokes-=1
-	if (pokerID ~= haspoked[pokerID]) then --Checks if there's a "record" of the poker
-			haspoked[pokerID] = pokerID --Creates a record
-			count[pokerID] = 1 --Sets his poke count to one
-			poketime[pokerID] = os.time() --Records the time of first poke
-			if (doreply == "y") then
-				ts3.requestSendPrivateTextMsg(serverConnectionHandlerID, recmsg, pokerID) --Send message back.
-			end
-	elseif(pokerID == haspoked[pokerID])then --If there is a record already
-		  if (count[pokerID] >= maxpokes and os.difftime(poketime[pokerID],os.time()) <= maxtime) then --If the poker has poked over -maxpokes- times under maxtime interval.
-			  ts3.requestClientKickFromServer(serverConnectionHandlerID,pokerID, kickmsg) --Kick poker with message.
-			  poketime[pokerID] = os.time() --Reset timer.
+	if (pokerID ~= haspoked[pokerID]) then
+			haspoked[pokerID] = pokerID
+			count[pokerID] = 1
+			poketime[pokerID] = os.time()
+			ts3.requestSendPrivateTextMsg(serverConnectionHandlerID, "I've received your poke. I'll get back to you as time allows.", pokerID)
+	elseif(pokerID == haspoked[pokerID])then
+		  if (count[pokerID] >= maxpokes and os.difftime(poketime[pokerID],os.time()) <= maxtime) then
+			  ts3.requestClientKickFromServer(serverConnectionHandlerID,pokerID,"Don't abuse the poke functionality please! ")
+			  poketime[pokerID] = os.time()
 			else
-				if(os.difftime(poketime[pokerID],os.time()) > maxtime) then --If the interval between pokes is greater than maxtime.
-					poketime[pokerID] = os.time --Reset timer.
+				if(os.difftime(poketime[pokerID],os.time()) > maxtime) then
+					poketime[pokerID] = os.time
 				end
-				if (doreply == "y")then
-				  ts3.requestSendPrivateTextMsg(serverConnectionHandlerID, recmsg, pokerID) --Sends message.
-				end
-				count[pokerID] = count[pokerID]+1 --Increments poke count for the user.
+				ts3.requestSendPrivateTextMsg(serverConnectionHandlerID, "I've received your poke. I'll get back to you as time allows.", pokerID)
+				count[pokerID] = count[pokerID]+1
 	  	end
 	end
 end
